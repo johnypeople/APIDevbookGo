5c07417655e0e8ee2c1ec57eb8f43efa1a7470e1
johnypeople <gjoao18@gmail.com>
2021-03-18 18:53:24 -0300

    Add user formating func

--- api/src/models/usuario.go
+++ api/src/models/usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //Usuario representa um usuario
 type Usuario struct {
@@ -10,4 +14,36 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+// Preparar vai chamar os outros dois métodos validar e fornatar
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco.")
+	}
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em branco.")
+	}
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório e não pode estar em branco.")
+	}
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatório e não pode estar em branco.")
+	}
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
