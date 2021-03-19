5e7ce34e7d1e4b487ead7bacaf1688432201edaa
johnypeople <gjoao18@gmail.com>
2021-03-19 01:47:02 -0300

    Adding bcrypt package with hash into the password

---
+++ api/src/seguranca/seguranca.go
@@ -0,0 +1,13 @@
+package seguranca
+
+import "golang.org/x/crypto/bcrypt"
+
+//go get golang.org/x/crypto/bcrypt <- tratar hash
+
+func Hash(senha string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
+}
+
+func VerificarSenha(senhaComHash, senhaString string) error {
+	return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senhaString))
+}
